88d778394b2cf2b0e9b66606d04452a3d06f0064
Michael Krein <michaelkrein2@gmail.com>
2023-02-27 15:52:08 -0600

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
+task("accounts", "Prints the list of accounts", async (taskArks, hre) => {
+  const accounts = await hre.ethers.getSigner();
+
+  for (const accounts of accounts) {
+    console.log(accounts.address);
+  }
+});
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    localhost: {},
+  },
 };
