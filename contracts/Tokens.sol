f80f0f73b003617c61d8a5f3b3941d91b883857f
Michael Krein <michaelkrein2@gmail.com>
2023-03-01 23:02:03 -0600

    6. Token approvals

--- contracts/Tokens.sol
+++ contracts/Tokens.sol
@@ -11,11 +11,18 @@ contract Token {
 
     
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
    
 
-   event Transfer(
+    event Transfer(
         address indexed from,
         address indexed to,
+        uint256 value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
         uint256 value
     );
 
@@ -33,8 +40,7 @@ contract Token {
 
     function transfer(address _to, uint256 _value) 
         public
-        returns
-        (bool success) 
+        returns(bool success) 
     {
     
         require(balanceOf[msg.sender] >= _value);
@@ -48,4 +54,26 @@ contract Token {
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
+
+    function approve(address _spender, uint256 _value) 
+        public
+        returns(bool success) 
+        {
+            require(_spender != address(0));
+            allowance[msg.sender][_spender] = _value;
+
+            emit Approval(msg.sender, _spender, _value);
+            return true;
+        }
+
+
+
+
+
+
+
+
+
+
+
 }
