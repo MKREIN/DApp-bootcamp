9afb389912cf395780324be1f82a950acbaeebda
Michael Krein <michaelkrein2@gmail.com>
2023-03-04 22:47:00 -0600

    11. Make orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,6 +8,10 @@ contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public orderCount;
+   
+
 
     event Deposit(
         address token,
@@ -22,6 +26,28 @@ contract Exchange {
         uint256 amount,
         uint256 balance
     );
+
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    // A way to model the order
+    struct _Order {
+        // Attributes of an order
+        uint256 id; // Unique identifier for order
+        address user; // User who made order
+        address tokenGet; // Address of the token the receive
+        uint256 amountGet; // Amount the receive
+        address tokenGive; // Address of the token they give
+        uint256 amountGive; // Amount they give
+        uint256 timestamp; // When order was created
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -65,12 +91,49 @@ contract Exchange {
         }
 
 
+    // Make & Cancel Orders
+    //---------------------
+     function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) public {
+        // Prevent orders if tokens aren't on exchange
+        require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+
+        // Instantiate a new order
+        orderCount = orderCount + 1;
+        orders[orderCount] = _Order (
+            orderCount, 
+            msg.sender, 
+            _tokenGet,
+            _amountGet, 
+            _tokenGive, 
+            _amountGive, 
+            block.timestamp  
+        );
+
+        // Emit event
+        emit Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+
+    }
+
 }
 
     // [x] Deposit Tokens
     // [x] Withdraw Tokens
     // [x] Check Balances
-    // [] Make Orders
+    // [x] Make Orders
     // [] Cancel Orders
     // [] Fill Orders
     // [] Charge Fees
