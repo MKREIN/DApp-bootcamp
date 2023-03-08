f48a4d79b87ef5672b4b21e8e4aff6ec6e0c5369
Michael Krein <michaelkrein2@gmail.com>
2023-03-07 23:00:29 -0600

    15. UI Setup

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,15 +1,34 @@
 async function main() {
+  console.log(`preparing deployment...\n`);
   //Fetch contract to deploy
   const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
+
+  //Fetch accounts
+  const accounts = await ethers.getSigners();
+
+  console.log(
+    `Accounts fetched: \n${accounts[0].address}\n${accounts[1].address}\n`
+  );
 
   //Deploy contract
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log(`Token Deployed to: ${token.address}`);
+  const Nox = await Token.deploy("Nox", "NOX", "1000000000");
+  await Nox.deployed();
+  console.log(`NOX Deployed to: ${Nox.address}`);
+
+  const Spud = await Token.deploy("Spud", "SPUD", "1000000000");
+  await Spud.deployed();
+  console.log(`SPUD Deployed to: ${Spud.address}`);
+
+  const NFL = await Token.deploy("NFL Token", "NFL", "100000000000");
+  await NFL.deployed();
+  console.log(`NFL Deployed to: ${NFL.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 5);
+  await exchange.deployed();
+  console.log(`Exchange Deployed to ${exchange.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
